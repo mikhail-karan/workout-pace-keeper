39e1549599c0009a8f7b7daeace64e8dcf0cdf07
Mikhail Karan <info@digitaldynasty.ca>
2018-03-04 15:38:35 -0500

    -repo setup

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    new MaterialApp(
+      title: 'Interval Buzzer',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Interval Buzzer'),
+        ),
+      ),
+    ),
+  );
+}
